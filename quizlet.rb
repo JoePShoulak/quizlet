f9db02ea9fca2b9109e9fc900e53d3e1e412dbfd
joepshoulak <joepshoulak2@gmail.com>
2014-07-05 21:00:03 -0400

    fix repeatable problems

--- quizlet.rb
+++ quizlet.rb
@@ -16,118 +16,9 @@ class Question
   def initialize(difficulty)
     @diff = difficulty
   end
-  
-  def addition
-    a = rand(10)
-    b = rand(10)
-    c = a + b
-    puts "#{a}+#{b}=x"
-    print "x="
-    answer = gets.chomp!.to_i
-    if c == answer
-      ["addition", true]
-    else
-      ["addition", c]
-    end
-  end
-  
-  def subtraction
-    a = rand(10)
-    b = rand(10)
-    c = a - b
-    puts "#{a}-#{b}=x"
-    print "x="
-    answer = gets.chomp!.to_i
-    if c == answer
-      ["subtraction", true]
-    else
-      ["subtraction", c]
-    end
-  end
-  
-  def multiplication
-    a = rand(10)
-    b = rand(10)
-    c = a * b
-    puts "#{a}X#{b}=x"
-    print "x="
-    answer = gets.chomp!.to_i
-    if c == answer
-      ["multiplication", true]
-    else
-      ["multiplication", c]
-    end
-  end
 
-  def division
-    b = rand(10)
-    c = rand(9) + 1
-    a = b * c
-    puts "#{a}/#{b}=x"
-    print "x="
-    answer = gets.chomp!.to_i
-    if c == answer
-      ["division", true]
-    else
-      ["division", c]
-    end
-  end
   
-  def squares
-    a = rand(10)
-    b = a**2
-    puts "#{a}^2=x"
-    print "x="
-    answer = gets.chomp!.to_i
-    if b == answer
-      ["squares", true]
-    else
-      ["squares", b]
-    end
-  end
-
-  def roots
-    b = rand(10)
-    a = b**2
-    puts "√#{a}=x"
-    print "x="
-    answer = gets.chomp!.to_i
-    if b == answer
-      ["roots", true]
-    else
-      ["roots", b]
-    end
-  end
-
-  def exponents
-    a = rand(5)
-    b = rand(3..5)
-    c = a**b
-    puts "#{a}^#{b}=x"
-    print "x="
-    answer = gets.chomp!.to_i
-    if c == answer
-      ["exponents", true]
-    else
-      ["exponents", c]
-    end
-  end
-
-  def logarithms
-    a = rand(2..5)
-    c = rand(5)
-    b = a**c
-    puts "#{a}^x=#{b}"
-    print "x="
-    answer = gets.chomp!.to_i
-    if c == answer
-      ["logarithms", true]
-    else
-      ["logarithms", c]
-    end
-  end
-  
-  def ask
+  def generate
     case @diff
     when "easy"
       cattegories = ["addition", "subtraction", "multiplication", "division"]
@@ -137,7 +28,44 @@ class Question
       cattegories = ["exponents", "logarithms"]
     end
     
-    self.send(cattegories[rand(cattegories.length)])
+    a = rand(10)
+    b = rand(10)
+     
+    type = cattegories[rand(cattegories.length)]
+    case type
+    when "addition"
+      correct_answer = a + b
+      question = "#{a}+#{b}=x"
+    when "subtraction"
+      correct_answer = a - b
+      question = "#{a}-#{b}=x"
+    when "multiplication"
+      correct_answer = a * b
+      question = "#{a}X#{b}=x"
+    when "division"
+      correct_answer = a
+      b = (b % 10) + 1
+      a = a * b
+      question = "#{a}/#{b}=x"
+    when "squares"
+      correct_answer = a**2
+      question = "#{a}^2=x"
+    when "roots"
+      correct_answer = a
+      a = a**2
+      question = "√#{a}=x"
+    when "exponents"
+      a, b = a % 5 + 2 , b % 5
+      correct_answer = a**b
+      question = "#{a}^#{b}=x"
+    when "logarithms"
+      a, b = a % 5 + 1, b % 5
+      correct_answer = b
+      b = a**b
+      question = "#{a}^x=#{b}"
+    end
+    
+    return [question, correct_answer]
   end
 end
 
@@ -149,18 +77,28 @@ class Quizlet
     questions[:medium] ||= 0
     questions[:hard]   ||= 0
     
+    questions[:easy] %= 20
+    questions[:medium] %= 20
+    questions[:hard] %= 20
+    
+    prev_questions = []
+    
     puts "Welcome to QUIZLET!"
     
     questions.keys.each do |k|
       questions[k].times do
         a = Question.new(k.to_s)
-        result = a.ask
-        if result[1] == true
+        to_ask = a.generate
+        while prev_questions.include? to_ask  do
+          to_ask = a.generate
+        end
+        puts to_ask[0]
+        user_answer = gets.chomp!.to_i
+        if user_answer == to_ask[1] 
           puts "Correct!"
           score += 1
         else
-          puts "Sorry, the answer was #{result[1]}."
-          wrong += [result[0]]
+          puts "Sorry, the answer was #{to_ask[1]}."
         end
       end
     end  
@@ -172,11 +110,11 @@ class Quizlet
     when percent > 80
       puts "Good!"
     when percent > 70
-      puts "Not bad, but maybe work on your #{most_common_value(wrong)}. (#{wrong.count(most_common_value(wrong))} wrong)"
+      puts "Not bad."
     when percent > 60
-      puts "Could use same work? Maybe take a look at your #{most_common_value(wrong)}. (#{wrong.count(most_common_value(wrong))} wrong)"
+      puts "Could use same work?"
     else
-      puts "Maybe try again? Also, you should focus on your #{most_common_value(wrong)}. (#{wrong.count(most_common_value(wrong))} wrong)"
+      puts "Maybe try again?"
     end
   end
 end
