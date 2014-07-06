9c11763d6a4bf0968069c74f37ad490f0734554e
joepshoulak <joepshoulak2@gmail.com>
2014-07-06 15:06:34 -0400

    Refactor, add word problems with random names

--- quizlet.rb
+++ quizlet.rb
@@ -13,9 +13,23 @@ def most_common_value(a)
 end
 
 class Question
-  def initialize(difficulty)
+  def initialize(difficulty, word_problem=false)
     @diff = difficulty
+    @word_problem = word_problem
   end
+  
+  def random_name
+    f = File.open("./names.txt")
+    names = f.readline().split("\n")
+    names.sample
+  end
+  
+  def wordify(a, b, file)
+    f = File.open("./word_#{file}.txt")
+    word_problems = f.readline().split("\n")
+    word_problems.sample.sub("X", a.to_s).sub("Y", b.to_s).gsub("NAME_1", random_name()).gsub("NAME_2", random_name())
+  end
+
 
   
   def generate
@@ -28,41 +42,73 @@ class Question
       cattegories = ["exponents", "logarithms"]
     end
     
-    a = rand(10)
-    b = rand(10)
+    a = rand(2..12)
+    b = rand(2..12)
      
-    type = cattegories[rand(cattegories.length)]
+    type = cattegories.sample
     case type
     when "addition"
       correct_answer = a + b
-      question = "#{a}+#{b}=x"
+      if @word_problem
+        question = wordify(a, b, "a")
+      else
+        question = "#{a}+#{b}=x, x=?"
+      end
     when "subtraction"
+      a, b = [a, b].max, [a, b].min
       correct_answer = a - b
-      question = "#{a}-#{b}=x"
+      if @word_problem
+        question = wordify(a, b, "s")
+      else
+        question = "#{a}-#{b}=x, x=?"
+      end
     when "multiplication"
       correct_answer = a * b
-      question = "#{a}X#{b}=x"
+      if @word_problem
+        question = wordify(a, b, "m")
+      else
+        question = "#{a}X#{b}=x, x=?"
+      end
     when "division"
       correct_answer = a
-      b = (b % 10) + 1
       a = a * b
-      question = "#{a}/#{b}=x"
+      if @word_problem
+        question = wordify(a, b, "d")
+      else
+        question = "#{a}/#{b}=x, x=?"
+      end
     when "squares"
       correct_answer = a**2
-      question = "#{a}^2=x"
+      if @word_problem
+        question = wordify(a, b, "q")
+      else
+        question = "#{a}^2=x, x=?"
+      end
     when "roots"
       correct_answer = a
       a = a**2
-      question = "√#{a}=x"
+      if @word_problem
+        question = wordify(a, b, "r")
+      else
+        question = "√#{a}=x, x=?"
+      end
     when "exponents"
       a, b = a % 6 + 2 , b % 6
       correct_answer = a**b
-      question = "#{a}^#{b}=x"
+      if @word_problem
+        question = wordify(a, b, "e")
+      else
+        question = "#{a}^#{b}=x, x=?"
+      end
     when "logarithms"
       a, b = a % 6 + 1, b % 6
       correct_answer = b
       b = a**b
-      question = "#{a}^x=#{b}"
+      if @word_problem
+        question = wordify(a, b, "l")
+      else
+        question = "#{a}^x=#{b}, x=?"
+      end
     end
     
     return [question, correct_answer]
@@ -70,7 +116,7 @@ class Question
 end
 
 class Quizlet
-  def run(questions)
+  def run(questions, word_problem)
     score = 0
     wrong = []
     questions[:easy]   ||= 0
@@ -87,13 +133,15 @@ class Quizlet
     
     questions.keys.each do |k|
       questions[k].times do
-        a = Question.new(k.to_s)
+        a = Question.new(k.to_s, word_problem)
         to_ask = a.generate
         while prev_questions.include? to_ask  do
           to_ask = a.generate
         end
-        puts to_ask[0]
-        user_answer = gets.chomp!.to_i
+        prev_questions += [to_ask]
+        puts "  " + to_ask[0]
+        print "Answer: "
+        user_answer = gets.gsub(/[^0-9]/, "").to_i
         if user_answer == to_ask[1] 
           puts "Correct!"
           score += 1
@@ -120,4 +168,4 @@ class Quizlet
 end
 
 q = Quizlet.new
-q.run({easy:5, medium:5, hard:5})
+q.run({easy:5, medium:5, hard:5}, true)
